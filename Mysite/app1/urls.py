e6f3c9bd4c6be2edd9909852fc3aca96c18e0a35
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2018-10-27 17:46:39 +0300

    Create page "Contacts"

--- Mysite/app1/urls.py
+++ Mysite/app1/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from app1.views import index, empt_page
+from app1.views import index, empt_page,contact
 
 urlpatterns = [
     path('', index),
     path('app1/', empt_page),
+    path('contact/', contact),
 ]
--- Mysite/app1/views.py
+++ Mysite/app1/views.py
@@ -4,7 +4,13 @@ from django.http import HttpResponse
 def index(request):
     return render(request, "app1/homePage.html")
 
+def contact(request):
+    return render(request,"app1/basic.html",{'values':[
+        'Усли у вас остались вопросы, то задавайте их по телефону',
+        '(025) 948-43-09',
+        'aln@mail.com'
+        ]})
 
-def empt_page(request):
+def empt_page(request):# empty page for "app1"
     return HttpResponse('<H3>It is page "app1"</H3>')
 
